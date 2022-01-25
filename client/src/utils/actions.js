fb66dda1afe1d2f0817d5ca65d476b462a5e2248
Lauren Groh <legroh@uwm.edu>
2022-01-24 21:29:29 -0600

    create and test actions and reducers

---
+++ client/src/utils/actions.js
@@ -0,0 +1,9 @@
+// define actions (events that can be emitted to update state)
+// store product/category data in global state
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+// select category & display products
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
+
+
+
